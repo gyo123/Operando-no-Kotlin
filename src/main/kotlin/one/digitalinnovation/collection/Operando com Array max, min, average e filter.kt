85fec2f10e6aae458e1a9fa209f1345dd8b01b0d
gyo123 <gyo1232010@hotmail.com>
2021-02-25 09:17:16 -0300

    Operando com array count, find e any, refatorando operações maxOrNull e minOrNull,

---
+++ src/main/kotlin/one/digitalinnovation/collection/Operando com Array max, min, average e filter.kt
@@ -0,0 +1,30 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val Number = doubleArrayOf(1000.0, 2000.0)
+
+    println("Maior salário: ${Number.max()}")
+    println("Menor Salário: ${Number.min()}")
+    println("Média salarial: ${Number.average()}")
+
+    val SalarioMaiorQue1500 = Number.filter { it > 1500.0 }
+    SalarioMaiorQue1500.forEach{ println(it)}
+
+    print("-----------------")
+    // duas formas de usar o count exemplos
+
+    println("\n Numero de elementos que estão dentro da array e são maior que 500.0 e menor que 3000.0 é " + Number.count{ it in 500.0..1500.0})
+    val count2 = Number.count{it in 500.0..1500.0}
+    println(count2)
+
+    print("----------------- \n")
+    println(Number.find { it == 1000.0 })
+    println(Number.find { it == 500.0 })
+    val find2 = Number.find { it == 2000.0 }
+    print(find2)
+
+    print("\n ----------------- \n")
+//retorna true or false
+    println(Number.any{it == 1000.0})  //true
+    println(Number.any{it == 500.0})   //false
+}
--- src/main/kotlin/one/digitalinnovation/collection/TesteIntArrayOf.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteIntArrayOf.kt
@@ -5,6 +5,17 @@ fun main() {
     val z = arrayOf("Hello", "gabriel", "Mundo")
     z.forEach { println(it) }
     println(" \n ----------------- \n")
+    val q = DoubleArray(5)
+
+    q[0] = 10000.0
+    q[1] = 3000.0
+    q[2] = 5000.0
+    q[3] = 20000.0
+    q[4] = 6000.0
+    q.sort()
+    q.forEach { i -> print("$i (Gyo te amo) \n") }
+
+    println(" \n ----------------- \n")
 
     for (valu in values){
         println(valu)
